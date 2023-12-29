3b63f7b7a89e0a22d88273a337f1feec20ce7aac
ErinTomorri <97967950+ErinTomorri@users.noreply.github.com>
2023-12-28 19:35:30 -0500

    Add files via upload

---
+++ main (fix).py
@@ -0,0 +1,57 @@
+
+import sys
+import compare
+#import wallet #code for saving data
+import httphandler
+from threading import Thread
+import objectCache
+import time
+import apiCall
+import black
+import scrape
+from price import price
+
+compare_threads = []
+
+def startDacompare():
+    global compare_threads
+    gold_end = compare.compare(\
+            objectCache.CachedDict().Traded['Gold']['End'],\
+            5,\
+            100000,\
+            30,\
+            "GoldEnd")
+    silver_end = compare.compare(\
+            objectCache.CachedDict().Traded['Silver']['End'],\
+            5,\
+            100000,\
+            30,\
+            "SilverEnd")
+    dow_above = compare.compare(\
+            objectCache.CachedDict().Traded['Dow']['Above'],\
+            5,\
+            100000,\
+            30,\
+            "DowAbove")
+    thread_gold = Thread(target = gold_end.compare, args = [])
+    thread_silver = Thread(target = silver_end.compare, args = [])
+    thread_gold.daemon = True
+    thread_silver.daemon = True
+    thread_gold.start()
+    time.sleep(60)
+    thread_silver.start()
+
+def threadManager():
+    # Reads in config file every half a second, see if anything changes
+    # Apply changes accordingly like deleting threads
+    f = open('../log/compare.log', 'r')
+    f.readline()
+    param_name = (f.readline()).split(',')
+    
+
+if __name__ == "__main__":
+    price() # test
+    startDacompare()
+    #displayThreads()
+    while(1):
+        time.sleep(1)
